62449efaddb148ba0d9a85e5cb43dddf80263e71
Emmanuel Públio <99607118+EPublio@users.noreply.github.com>
2023-06-10 15:05:34 -0300

    Update conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -6,6 +6,5 @@ $bd = '';
 $conexao = new \PDO('mysql:host=localhost; dbname='.$bd, $user_bd, $senha_bd);
 
 } catch (\PDOExeptiom $e) {
-file_put_contents("log.txt", $e->getMassage(), FILE_APPEND | LOCK_EX );
-echo '<div>Erro</div>';
+echo '<div>'.$e->getMassage().'</div>';
 }
